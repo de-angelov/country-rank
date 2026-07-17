23e6a4a98281ecef6d9afd9de6a7ea5132f4973a
denis <de.angelov.bg@gmail.com>
2026-07-18 00:41:59 +0300

    Reduce country card SVG markup

--- app/routes/home/home.test.tsx
+++ app/routes/home/home.test.tsx
@@ -325,6 +325,7 @@ describe("Home", () => {
     expect(html).toContain("Search countries");
     expect(html).toContain("country-filter-transition");
     expect(html).toContain('id="country-search"');
+    expect(html.match(/<svg/g)?.length).toBe(1);
     expect(visibleText(html)).not.toContain("Showing");
 
     for (const country of countries) {
